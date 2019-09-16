cd97df159e197380c69618f3016dc05d44657977
Bryce Thorson <brycethorson.design@gmail.com>
2019-09-16 13:47:05 -0400

    added checkbox functionality and styles for terms and conditions acceptance.

--- src/components/OnBoarding.vue
+++ src/components/OnBoarding.vue
@@ -16,6 +16,13 @@
           <li v-for="(_, index) in slides" :key="index" class="indicator" v-bind:class="{active: currentIndex === index}"></li>
         </ul>
     </div>
+    <div class="checkbox-panel">
+      <label for="checkbox" class="checkbox-container">
+        I accept the terms &amp; conditions
+        <input type="checkbox" id="checkbox" v-model="checked">
+        <span class="checkmark"></span>
+      </label>
+    </div>
   </div>    
 </template>
 
@@ -52,6 +59,7 @@
           }
         ],
         currentIndex: 0,
+        checked: false,
       }
     },
     methods: {
@@ -81,6 +89,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '../scss/_variables.scss';
+
 .onboarding {
   padding-top: 2rem;
   position: absolute;
@@ -90,14 +100,12 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
 }
 .info-panel {
-  padding-top: 30%;
   position: relative;
   width: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+  height: 200px;
   & .carousel {
     overflow: hidden;
     width: 90%;
@@ -111,12 +119,31 @@
     width: 100%;
     position: absolute;
   }
-  & .indicators {
-    bottom: -120px;
+  & .navigation {
     position: absolute;
+    color: white;
+    height: 200px;
+    left: -20px;
+    right: -20px;
+    & span {
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      width: 100px;
+    }
+    & span:first-of-type {
+      left: -1rem;
+    }
+    & span:last-of-type {
+      right: -1rem;
+    }
+  }
+  & .indicators {
     display: flex;
     flex-direction: row;
     width: 100%;
+    padding-top: 7rem;
     & .indicator {
       width: 10px;
       height: 10px;
@@ -128,6 +155,62 @@
       &.active {
         opacity: 1;
       }
+    }
+    & .indicator:first-of-type {
+      margin-left: 0.25rem;
+    }
+  }
+}
+.checkbox-panel {
+  color: white;
+  margin-bottom: 8rem;
+  display: flex;
+  flex-direction: row;
+  & .checkbox-container {
+      position: relative;
+      padding-left: 2.25rem;
+      cursor: pointer;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      & input {
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+        position: absolute;
+    }
+    & .checkmark {
+      position: absolute;
+      top: 2px;
+      left: 0;
+      height: 1rem;
+      width: 1rem;
+      background-color: $gray--100;
+      border-radius: 2px;
+      &::after {
+        content: "";
+        position: absolute;
+        display: none;
+      }
+    }
+    & input:checked ~ .checkmark {
+      background-color: white;
+    }
+    & input:checked ~ .checkmark:after {
+        display: block;
+    }
+    & .checkmark::after {
+      left: 5px;
+      top: 2px;
+      width: 6px;
+      height: 10px;
+      border: solid $secondaryBlue;
+      border-width: 0 2px 2px 0;
+      -webkit-transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      transform: rotate(45deg);
     }
   }
 }
@@ -142,27 +225,5 @@ h3, p {
 .logo {
   max-width: 300px;
   height: auto;
-  padding-bottom: 2rem;
-}
-.navigation {
-  position: absolute;
-  color: white;
-  height: 200px;
-  bottom: -164px;
-  left: -20px;
-  right: -20px;
-  & span {
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 100px;
-  }
-  & span:first-of-type {
-    left: -1rem;
-  }
-  & span:last-of-type {
-    right: -1rem;
-  }
 }
 </style>
